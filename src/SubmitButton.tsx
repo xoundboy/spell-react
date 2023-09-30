9b76857380884642aab62c834975aca7a432bb62
Ben Roberts <xoundboy@gmail.com>
2023-09-30 09:32:53 +0200

    initial commit

---
+++ src/SubmitButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SubmitButton() {
+    return (
+        <button>Submit</button>
+    );
+}
+
+export default SubmitButton;
---
+++ src/Word.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Word() {
+    return (
+        <div>Word</div>
+    );
+}
+
+export default Word;
