e0fe71d27a1955a2577af03f429ee473559b13e3
Ben Roberts <xoundboy@gmail.com>
2023-10-31 23:47:33 +0100

    cleanup

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const config: Record<string, any> = {
     REMOVAL_RATIO: 3,
     INITIAL_COUNTDOWN_SECONDS: 7,
     WORDS_PER_GAME: 30,
-    LOGGING_ENABLED: true,
+    LOGGING_ENABLED: false,
     LOCAL_STORAGE_KEY: 'spell-it-in-data',
     MAX_HIGH_SCORES: 5,
     CORRECT_ANSWER_OVERLAY_TIMEOUT: 2000,
